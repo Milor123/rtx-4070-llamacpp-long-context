f84295f2e9877969294f8ea6fcd41cfd3401b448
Milor123 <Milor123@users.noreply.github.com>
2026-06-20 13:54:41 -0500

    fix: use env var for chat-template-kwargs + cmd /c to avoid PS5.1 quoting bugs

--- configs/gemma-4-26b-256k.ps1
+++ configs/gemma-4-26b-256k.ps1
@@ -4,15 +4,9 @@ $buildDir = "C:\Users\User\Documents\TEO\atomic-llama-cpp-turboquant\build\bin\R
 $model    = "C:\Users\User\.vllm\gemma-4-26B-A4B-it-qat-UD\gemma-4-26B-A4B-it-qat-UD-Q4_K_XL.gguf"
 $template = Join-Path $PSScriptRoot "..\templates\3.6_chat_template-v10.jinja"
 
+$env:LLAMA_CHAT_TEMPLATE_KWARGS = '{"preserve_thinking": true}'
 Set-Location $buildDir
 
-& ".\llama-server.exe" -m $model `
-  --host 127.0.0.1 --port 10000 -ngl 99 -c 256000 -b 8192 -ub 2048 `
-  --no-mmap --direct-io --temp 1.0 -np 1 -fa on `
-  --top-k 64 --top-p 0.95 --min-p 0 `
-  --cont-batching --metrics `
-  --chat-template-kwargs '{"preserve_thinking": true}' `
-  -ctv turbo3 -ctk turbo3 --jinja -tb 14 -t 14 --poll 100 `
-  --cpu-strict 1 --n-cpu-moe 14
+cmd /c ".\llama-server.exe -m `"$model`" --host 127.0.0.1 --port 10000 -ngl 99 -c 256000 -b 8192 -ub 2048 --no-mmap --direct-io --temp 1.0 -np 1 -fa on --top-k 64 --top-p 0.95 --min-p 0 --cont-batching --metrics -ctv turbo3 -ctk turbo3 --jinja -tb 14 -t 14 --poll 100 --cpu-strict 1 --n-cpu-moe 14 --chat-template-file `"$template`""
 
 pause
